737a7c0f9c34b870aba52e67efb301871cea4b82
Alexander Rodgers <alexedrodgers@gmail.com>
2020-04-02 20:04:55 -0700

    added part of a dialog, want to refactor giveride component

--- src/components/RideCard.js
+++ src/components/RideCard.js
@@ -15,8 +15,15 @@ import Avatar from '@material-ui/core/Avatar';
 
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+
 import moment from 'moment';
 import { db, auth } from '../firebase';
+import { useMediaQuery } from '@material-ui/core';
+import { useTheme } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
    expand: {
@@ -35,6 +42,9 @@ const RideCard = (props) => {
    const [expanded, setExpanded] = useState(false);
    const [select, setSelect] = useState(false);
    const [option, setOption] = useState('');
+   const [dialog, setDialog] = useState(false);
+   const theme = useTheme();
+   const fullscreen = useMediaQuery(theme.breakpoints.down('sm'));
 
    let same = false;
    let id = props.id.split('-')[1];
@@ -82,72 +92,83 @@ const RideCard = (props) => {
    const classes = useStyles();
 
    return (
-      <Card 
-         style={{margin: '0px 5px', flex: 1, marginBottom: '5px'}}
-         key={props.id}
-         className="ride-card"
-      >  
-         <CardHeader
-            style={{paddingBottom: '0px'}}
-            avatar={
-               <Avatar aria-label="username" style={{backgroundColor:'blue'}}>
-                  A
-               </Avatar>
-            }
-            title={props.data.name}
-            subheader={`${getArea()} ${getDate(props.data.date)} ${getTime(props.data.time)}`}
-            action={
-               <IconButton onClick={() => getRoute()}>
-                  <ArrowUpwardIcon/>
-               </IconButton>
-            }
-         />
-         <CardActions disableSpacing>
-            <span style={{paddingLeft: '8px'}}>Details</span>
-            <IconButton
-               className={clsx(classes.expand, {
-                  [classes.expandOpen]: expanded,
-               })}
-               onClick={() => setExpanded(!expanded)}
-            >
-               <ExpandMoreIcon/>
-            </IconButton>
-            <div style={{flexGrow: '1'}}></div>
-            { same ?
-               <Fragment>
-                  <Select
-                     style={{visibility: 'hidden'}}
-                     value={option}
-                     open={select}
-                     onClose={() => setSelect(false)}
-                     onOpen={() => setSelect(true)}
-                     onChange={(event) => handleOption(event)}
-                  >
-                     <MenuItem value={'edit'}>Edit</MenuItem>
-                     <MenuItem value={'delete'}>Delete</MenuItem>
-                  </Select>
-                  <IconButton aria-label="settings" onClick={() => setSelect(true)}>
-                     <MoreVertIcon />
+      <div>
+         <Dialog
+            fullscreen={fullscreen}
+            open={dialog}
+            onClose={() => setDialog(false)}
+         >
+            <DialogTitle>Edit Ride</DialogTitle>
+            <DialogContent>
+            </DialogContent>
+         </Dialog>
+         <Card 
+            style={{margin: '0px 5px', flex: 1, marginBottom: '5px'}}
+            key={props.id}
+            className="ride-card"
+         >  
+            <CardHeader
+               style={{paddingBottom: '0px'}}
+               avatar={
+                  <Avatar aria-label="username" style={{backgroundColor:'blue'}}>
+                     A
+                  </Avatar>
+               }
+               title={props.data.name}
+               subheader={`${getArea()} ${getDate(props.data.date)} ${getTime(props.data.time)}`}
+               action={
+                  <IconButton onClick={() => getRoute()}>
+                     <ArrowUpwardIcon/>
                   </IconButton>
-               </Fragment>
-            :
-               ''
-            }
-         </CardActions>
-         <Collapse in={expanded} timeout="auto" unmountOnExit>
-            <CardContent>
-               {props.data.type === 'drive' ?
+               }
+            />
+            <CardActions disableSpacing>
+               <span style={{paddingLeft: '8px'}}>Details</span>
+               <IconButton
+                  className={clsx(classes.expand, {
+                     [classes.expandOpen]: expanded,
+                  })}
+                  onClick={() => setExpanded(!expanded)}
+               >
+                  <ExpandMoreIcon/>
+               </IconButton>
+               <div style={{flexGrow: '1'}}></div>
+               { same ?
                   <Fragment>
-                     <Typography>{props.data.name} is offering a ride</Typography>
-                     <Typography>Seats: {props.data.seats}</Typography>  
+                     <Select
+                        style={{visibility: 'hidden'}}
+                        value={option}
+                        open={select}
+                        onClose={() => setSelect(false)}
+                        onOpen={() => setSelect(true)}
+                        onChange={(event) => handleOption(event)}
+                     >
+                        <MenuItem value={'edit'}>Edit</MenuItem>
+                        <MenuItem value={'delete'}>Delete</MenuItem>
+                     </Select>
+                     <IconButton aria-label="settings" onClick={() => setSelect(true)}>
+                        <MoreVertIcon />
+                     </IconButton>
                   </Fragment>
                :
-                  <Typography>{props.data.name} is looking for a ride</Typography>
+                  ''
                }
-               <Typography>Phone number: {props.data.number}</Typography>
-            </CardContent>
-         </Collapse>
-      </Card>
+            </CardActions>
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+               <CardContent>
+                  {props.data.type === 'drive' ?
+                     <Fragment>
+                        <Typography>{props.data.name} is offering a ride</Typography>
+                        <Typography>Seats: {props.data.seats}</Typography>  
+                     </Fragment>
+                  :
+                     <Typography>{props.data.name} is looking for a ride</Typography>
+                  }
+                  <Typography>Phone number: {props.data.number}</Typography>
+               </CardContent>
+            </Collapse>
+         </Card>
+      </div>
    );
 }
 
